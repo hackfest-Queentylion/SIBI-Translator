e11b5e2c02919fab97df99d1245af0396d5fccf2
Zaki <zakiamanullah215@gmail.com>
2024-01-11 19:32:23 +0700

    chore: trying to deploy

--- server/go.mod
+++ server/go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.16.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -23,8 +24,8 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.7.0 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,1 +1,59 @@
-package server
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allow all origins, adjust in production
+	},
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello world!",
+		})
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		ws, err := wsupgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			return
+		}
+		defer func(ws *websocket.Conn) {
+			err := ws.Close()
+			if err != nil {
+
+			}
+		}(ws)
+
+		for {
+			// Read
+			_, msg, err := ws.ReadMessage()
+			if err != nil {
+				break
+			}
+			// Print the message to the console
+			fmt.Printf("%s\n", msg)
+			// Write
+			err = ws.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				break
+			}
+		}
+	})
+
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
